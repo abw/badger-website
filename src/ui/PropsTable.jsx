10cf583668f9bbe6124f13160e90f270b634c49a
Andy Wardley <abw@wardley.org>
2024-08-27 14:02:24 +0100

    PropsTable component

---
+++ src/ui/PropsTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { codify } from '@/utils/Markup.jsx'
+import { isString } from '@abw/badger-utils'
+
+const PropsTable = ({items}) =>
+  <table className="wide shaded striped celled">
+    <thead>
+      <tr>
+        <th>Property</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      { items.map(
+        ([name, def, desc]) =>
+          <PropRow key={name} name={name} defaultValue={def} description={desc}/>
+      )}
+    </tbody>
+  </table>
+
+const PropRow = ({name, defaultValue, description}) =>
+  <tr>
+    <td className="font-mono">{name}</td>
+    <td className="font-mono">{defaultValue}</td>
+    { isString(description)
+      ? <td dangerouslySetInnerHTML={{__html: codify(description)}}/>
+      : <td>{description}</td>
+    }
+  </tr>
+
+export default PropsTable
