4220a3217efbf77071a7495cd69b62f467b63b6a
Andy Wardley <abw@wardley.org>
2024-08-27 14:01:23 +0100

    Sidebar menu and item classes

--- src/sidebar/Menu.jsx
+++ src/sidebar/Menu.jsx
@@ -3,13 +3,13 @@ import UILink from '@/ui/Link.jsx'
 import { classes } from '@abw/badger-react-ui'
 import { SiteConsumer } from '@/site/Context.jsx'
 
-
 export const SidebarMenu = SiteConsumer(
   ({
     sidebar={},
     sidebarClick,
     items,
-    menuClass=sidebar.menuClass ?? 'menu border bdr-1',
+    menuClass=sidebar.menuClass ?? 'menu',
+    itemClass=sidebar.menuItemClass ?? 'item',
     Link=UILink
   }) =>
     <div className={menuClass} onClick={sidebarClick}>
@@ -17,7 +17,7 @@ export const SidebarMenu = SiteConsumer(
         ({ className, ...item }) =>
           <Link
             key={item.to}
-            className={classes(className, 'item')}
+            className={classes(className, itemClass)}
             {...item}
           >
             {item.display}
@@ -27,33 +27,3 @@ export const SidebarMenu = SiteConsumer(
 )
 
 export default SidebarMenu
-
-/*
-const Menu = ({title, path, items, tocs, closeSidebar}) => {
-  const resolved = useResolvedPath()
-  const open = path && resolved.pathname.slice(0, path.length) === path
-
-  return (
-    <details className="menu" open={open}>
-      <summary>{title}</summary>
-      <ul>
-        { items.map(
-          item => {
-            const active = resolved.pathname === item.to
-            return (
-              <li key={item.to} onClick={closeSidebar}>
-                <Link className="item" {...item}/>
-                { (Boolean(item.tocName) && active && tocs[item.tocName]) &&
-                  <MenuTOC toc={tocs[item.tocName]}/>
-                }
-              </li>
-            )
-          }
-        )}
-      </ul>
-    </details>
-  )
-}
-*/
-
-// export default Context.Consumer(Menu)
