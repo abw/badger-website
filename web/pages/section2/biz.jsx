c88ae783c2e766d93adb179a45b3f0678aa5474c
Andy Wardley <abw@wardley.org>
2024-09-10 08:29:43 +0100

    Link styling tweaks

--- web/pages/section2/biz.jsx
+++ web/pages/section2/biz.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@abw/badger-react-ui'
 import React from 'react'
 
 export const metadata = {
@@ -7,6 +8,11 @@ export const metadata = {
 const Biz = () =>
   <div>
     <h1>The Biz Page</h1>
+    <div className="flex gap-2 mar-b-2">
+      <Button color="brand" text="Brand Button"/>
+      <a href="/" className="button">Link Button</a>
+      <a href="/" className="brand button">Brand Link Button</a>
+    </div>
   </div>
 
 export default Biz
