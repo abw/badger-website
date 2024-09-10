562527afd39d4b315efcaec5286b838368e106f7
Andy Wardley <abw@wardley.org>
2024-09-10 08:30:07 +0100

    Docs build

--- docs/assets/hello-C98P9wQu.js
+++ docs/assets/hello-C98P9wQu.js
@@ -1,2 +1,2 @@
-import{j as e}from"./index-xz4neTAR.js";function o(n){const t={h1:"h1",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Hello World!"}),`
+import{j as e}from"./index-wwXa3OIO.js";function o(n){const t={h1:"h1",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Hello World!"}),`
 `,e.jsx(t.p,{children:"This is an MDX example loaded by file name."})]})}function s(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{s as default};
