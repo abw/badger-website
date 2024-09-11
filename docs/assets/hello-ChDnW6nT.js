89e2c78b7c2a70bdd6520a24d61c50e2cd58858e
Andy Wardley <abw@wardley.org>
2024-09-11 09:56:46 +0100

    Docs build

--- docs/assets/hello-ChDnW6nT.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-v8jlX3P6.js";function o(n){const t={h1:"h1",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Hello World!"}),`
-`,e.jsx(t.p,{children:"This is an MDX example loaded by file name."})]})}function s(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{s as default};
---
+++ docs/assets/install-6JIVdJXe.js
@@ -0,0 +1,8 @@
+const s=`## using npm
+$ npm add -D @abw/badger-website sass @mdx-js/rollup vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props
+
+## using yarn
+$ yarn add -D @abw/badger-website sass @mdx-js/rollup vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props
+
+## using pnpm
+$ pnpm add -D @abw/badger-website sass @mdx-js/rollup vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props`;export{s as default};
