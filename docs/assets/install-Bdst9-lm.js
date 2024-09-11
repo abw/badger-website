22b399ffc0c1350f6e77fbfa9de30dd66b5aa7db
Andy Wardley <abw@wardley.org>
2024-09-11 11:58:36 +0100

    Docs build

--- docs/assets/install-Bdst9-lm.js
+++ docs/assets/install-Bdst9-lm.js
@@ -1,8 +1,8 @@
-## using npm
+const s=`## using npm
 $ npm add -D @abw/badger-website sass @mdx-js/rollup @mdx-js/react vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props
 
 ## using yarn
 $ yarn add -D @abw/badger-website sass @mdx-js/rollup @mdx-js/react vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props
 
 ## using pnpm
-$ pnpm add -D @abw/badger-website sass @mdx-js/rollup @mdx-js/react vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props
+$ pnpm add -D @abw/badger-website sass @mdx-js/rollup @mdx-js/react vite-plugin-svgr vite-jsconfig-paths rehype-mdx-code-props`;export{s as default};
