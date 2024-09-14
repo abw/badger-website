2823c65d12f9e4e6e7149787ce983f4a4c8c93e4
Andy Wardley <abw@wardley.org>
2024-09-14 10:54:45 +0100

    Snippet cleanup

---
+++ docs/assets/Provider-D8G_jDx2.js
@@ -0,0 +1,9 @@
+const o=`import React from 'react'
+import { SiteProvider } from '@abw/badger-website'
+
+const App = () =>
+  <SiteProvider
+    // ...configuration options
+  />
+
+export default App`;export{o as default};
