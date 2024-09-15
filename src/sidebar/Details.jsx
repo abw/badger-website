08d4008faec557930a61419d1818465c8d44151d
Andy Wardley <abw@wardley.org>
2024-09-15 12:01:29 +0100

    Sidebar details path match

--- src/sidebar/Details.jsx
+++ src/sidebar/Details.jsx
@@ -14,7 +14,7 @@ export const SidebarDetails = ({
 }) => {
   const { SidebarMenu=Menu } = useSite()
   const resolved = useResolvedPath()
-  const open = openPath && resolved.pathname.slice(0, openPath.length) === openPath
+  const open = isOpen(resolved.pathname, openPath)
   if (openPath) {
     // console.log(`openPath: ${openPath} vs [${resolved.pathname.slice(0, openPath.length)}]`)
   }
@@ -31,5 +31,21 @@ export const SidebarDetails = ({
   )
 }
 
+const isOpen = (resolved, openPath) => {
+  if (! openPath) {
+    return false
+  }
+  if (resolved === openPath) {
+    return true
+  }
+  // We have to be careful here because we might have two sections which are
+  // /foo and /food.  When we're matching /foo the next character must be a
+  // path separator or the end of string, otherwise we'll match /food
+  const slice = resolved.slice(0, openPath.length + 1)
+  if (slice === openPath || slice === openPath + '/') {
+    return true
+  }
+  return false
+}
+
 export default SidebarDetails
-// export default SiteConsumer(SidebarDetails)
