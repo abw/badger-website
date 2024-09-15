5cc38447e136420faaf84a785c5de1d166c44435
Andy Wardley <abw@wardley.org>
2024-09-15 11:09:59 +0100

    Docs build

--- docs/assets/hello-DpKlD22w.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DsgC5jvw.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Hello World!"}),e.jsx("p",{children:"This is a JSX example loaded by file name."})]});export{s as default};
