be576710acf016d031adab142b89301fb945b802
Andy Wardley <abw@wardley.org>
2024-09-15 11:14:31 +0100

    Docs build

--- docs/assets/hello-D-fNI_U7.js
+++ docs/assets/hello-D-fNI_U7.js
@@ -1,2 +1,2 @@
-import{u as s,j as e}from"./index-DmrfRidC.js";function o(n){const t={h1:"h1",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Hello World!"}),`
+import{u as s,j as e}from"./index-BfFMIBB-.js";function o(n){const t={h1:"h1",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Hello World!"}),`
 `,e.jsx(t.p,{children:"This is an MDX example loaded by file name."})]})}function a(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default};
