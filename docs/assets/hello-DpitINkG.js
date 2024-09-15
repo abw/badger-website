50a872492d68abcc2cc964dc0d7290b6d21179b3
Andy Wardley <abw@wardley.org>
2024-09-15 10:20:15 +0100

    Docs build

--- docs/assets/hello-DpitINkG.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C3uGdPzp.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Hello World!"}),e.jsx("p",{children:"This is a JSX example loaded by file name."})]});export{s as default};
