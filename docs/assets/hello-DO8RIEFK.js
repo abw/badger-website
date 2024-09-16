2a47e0de04e3a55127c62505f5ddfde3b6069aaa
Andy Wardley <abw@wardley.org>
2024-09-16 18:37:40 +0100

    Docs build

--- docs/assets/hello-DO8RIEFK.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B-13h4n0.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Hello World!"}),e.jsx("p",{children:"This is a JSX example loaded by file name."})]});export{s as default};
